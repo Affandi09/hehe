a264bc7d2f71037b70b33596c8fb634d25893a12
tommy1804 <48713990+tommy1804@users.noreply.github.com>
2019-12-11 15:28:00 +0700

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,48 +1,40 @@
 #include <iostream>
 using namespace std;
-int queue[10], n = 10, front = -1, rear = 0,arr[10];
-string nama[10];
-string alamat[10];
-void Insert() {
-   int val;
-   if (rear == n )
-      cout<<"Truk Habis"<<endl;
-   else {
-      if (front == - 1)
-      front = 1;
-      cout<<"Masukkan data : ";
-      cin>>val;
-      rear++;
-      queue[rear] = val;
-      for (int x=val; x<=rear;x++){
-			arr[x] = rear;
-			cout<<"Data ke: "<<rear<<endl;
-      		cout<<"Masukkan Nama Penerima: ";
-      		cin>>nama[x];
-      		cout<<"Masukkan Alamat Pengiriman: ";
-      		cin>>alamat[x];
-      		cout<<endl;
-	  	}
-   }
+void swap(string *a, string *b){
+	string temp = *a;
+	*a=*b;
+	*b=temp;
 }
 
-void Delete() {
-   if (front == - 1 || front > rear) {
-      cout<<"Tidak Ada Pesanan ";
-   return ;
-   } else {
-      cout<<"Pesanan Selesai : "<< queue[front] <<endl;
-      front++;;
-   }
+void sort(string a[10], string b[10], int n, int ch){
+	string temp;
+	int i;
+	if (ch==1) {
+		for(int x=0; x<(n-1); x++){
+			for(int y=(x+1); y<n; y++){
+				i=0;
+				while(int(a[x][i])==int(a[y][i]))
+					i++;
+				if(int(a[x][i])>int(a[y][i])){
+					swap(a[x],a[y]);
+					swap(b[x],b[y]);
+				}
+			}
+		}	
+	}
+	else {
+		for(int x=0; x<(n-1); x++){
+			for(int y=(x+1); y<n; y++){
+				i=0;
+				while(int(a[x][i])==int(a[y][i]))
+					i++;
+				if(int(b[x][i])>int(b[y][i])){
+					swap(a[x],a[y]);
+					swap(b[x],b[y]);
+				}
+			}
+		}
+	}
 }
-void Display() {
-   if (front == - 1)
-   cout<<"Tidak Ada Pesanan"<<endl;
-   else {
-      cout<<"Pesanan Menunggu : "<<endl;
-      for (int i = front; i <= rear; i++) {
-         cout<<queue[i]<<" "<<nama[i]<<" "<<alamat[i]<<endl;
-     }
-      cout<<endl;
-   }
-}
+
+
